4b0a9398be3cdceb338ed2c5bf5bb86721be2e48
LAPTOP-7NHIC52L\kohei <sugawara622@gmail.com>
2020-06-09 01:18:26 +0900

    プロジェクト ファイルを追加します。

---
+++ Q01/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace Q01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Q01 10進数で回文");
+
+            var num = 11;
+
+            while (num < 1000)
+            {
+                var dNum = num.ToString();
+                var bNum = Convert.ToString(num, 2);
+                var hNum = Convert.ToString(num, 8);
+                if (dNum == string.Concat(dNum.Reverse()) &&
+                    bNum == string.Concat(bNum.Reverse()) &&
+                    hNum == string.Concat(hNum.Reverse()))
+                {
+                    Console.WriteLine($"10進数、2進数、8進数のいずれで表現しても回文数となる数のうち10進数の10以上で最小の値は {num}");
+                    Console.WriteLine($"10進数:{num}");
+                    Console.WriteLine($" 2進数:{bNum}");
+                    Console.WriteLine($" 8進数:{hNum}");
+                    break;
+                }
+
+                num++;
+            }
+        }
+    }
+}
---
+++ Q02/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+
+namespace Q02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Q02 数列の四則演算");
+
+            var array = new string[] { "*", "/", "+", "-", "" };
+
+            DataTable dc = new DataTable();
+            for (int num = 1000; num < 10000; num++)
+            {
+                var str = num.ToString();
+                for (int i = 0; i < array.Length; i++)
+                {
+                    for (int j = 0; j < array.Length; j++)
+                    {
+                        for (int k = 0; k < array.Length; k++)
+                        {
+                            var exp = str[0] + array[i] +
+                                      str[1] + array[j] +
+                                      str[2] + array[k] +
+                                      str[3];
+                            if (exp.Length > 4)
+                            {
+                                var data = dc.Compute(exp, "").ToString();
+                                if (data == string.Concat(str.Reverse()))
+                                {
+                                    Console.WriteLine(str);
+                                    Console.WriteLine($"{exp} = {data}");
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
