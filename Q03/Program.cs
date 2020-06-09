afb841ea1515d50e991123de2e87be432969df75
LAPTOP-7NHIC52L\kohei <sugawara622@gmail.com>
2020-06-09 23:42:01 +0900

    Q3解いた

---
+++ Q03/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+
+namespace Q03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Q03 カードを裏返せ");
+            var cards = new Boolean[100]; // false:裏 true:表
+
+            for(int i = 1; i < 100; i++)
+            {
+                for (int j = i; j < 100; j += (1 + i))
+                {
+                    cards[j] = !cards[j];
+                }
+            }
+
+            for (int k = 0; k < cards.Length; k++)
+            {
+                if (!cards[k])
+                    Console.WriteLine(k+1);
+            }
+
+        }
+    }
+}
