5ea6a974010187cc3f68644620f5f80c49b53eec
LAPTOP-7NHIC52L\kohei <sugawara622@gmail.com>
2020-06-10 00:19:00 +0900

    Q4解いた

---
+++ Q04/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Q04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Q04 棒の切り分け");
+            
+            EchoResult(20, 3);
+            EchoResult(100, 5);
+        }
+
+        static void EchoResult(int lodLength, int saws)
+        {
+            Console.WriteLine($"{lodLength}cmの棒を{saws}人で切り分けると{CountCuttingLod(lodLength, saws)}回で切り分けられる。");
+        }
+
+        static int CountCuttingLod(int lodLength, int saws)
+        {
+            var x = 1;
+            var cnt = 0;
+            while (x < lodLength)
+            {
+                x += x < saws ? x : saws;
+                cnt++;
+            }
+            return cnt;
+        }
+    }
+}
