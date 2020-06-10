3721fb8a1ffe4c5baf394788b1ab9657e1acf316
LAPTOP-7NHIC52L\kohei <sugawara622@gmail.com>
2020-06-10 21:06:42 +0900

    Q5解いた

---
+++ Q05/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Q05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Q05 いまだに現金払い？");
+
+            var cnt = 0;
+
+            for (int i = 0; i <= 2; i++)
+            {
+                for (int j = 0; j <= 10; j++)
+                {
+                    for (int k = 0; k <= 15; k++)
+                    {
+                        for (int l = 0; l <= 15; l++)
+                        {
+                            if ((i + j + k + l) <= 15)
+                            {
+                                if ((i * 500 + j * 100 + k * 50 + l * 10) == 1000)
+                                {
+                                    cnt++;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine($"{cnt}通り");
+        }
+    }
+}
